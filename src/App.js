29176372cfdca380d1637345d2b912a07aee1ca5
Muhammad Daniyal <74719299+Muhammad-Daniyal-1@users.noreply.github.com>
2022-08-12 10:23:12 +0500

    initial git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News pageSize={4} />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/News/News.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import Spinner from "../Spinner/Spinner";
+import NewsItem from "./NewsItem";
+
+export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=721c9b7b9ec14a5db5d1341d703816ce&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  }
+
+  handlePrevious = async () => {
+    console.log("previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=721c9b7b9ec14a5db5d1341d703816ce&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+
+  handleNext = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=721c9b7b9ec14a5db5d1341d703816ce&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h1 className="my-3 text-center">Top Headlines </h1>
+        <div className="row">
+          {this.state.loading && <Spinner />}
+          { !this.state.loading && this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 20) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 60) : ""
+                  }
+                  imgurl={element.urlToImage}
+                  newsurl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className=" d-flex justify-content-between my-5">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevious}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNext}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
