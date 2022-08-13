deca46abc2c0ceac1bfea9579e119af89911d234
Ahmad0222 <03164322144ahmad@gmail.com>
2022-08-13 10:49:04 +0500

    work in office

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News pageSize="8" country="us" category="general" />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+}
