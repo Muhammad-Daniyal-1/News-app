bc31e5a1e683b0b53ab1c5a7abcc30255817be5d
Ahmad0222 <03164322144ahmad@gmail.com>
2022-08-13 18:13:12 +0500

    trying to add top bar loading

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 
 import React, { Component } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import News from "./components/News/News";
+import News  from "./components/News/News";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default class App extends Component {
   pageSize = 8;
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress });
+  }
   render() {
     return (
       <div>
